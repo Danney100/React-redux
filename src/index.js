833865ccb1ccaea1d44ba5c0481736e80a2a03c3
challenger772 <challenger772@outlook.com>
2020-02-24 05:22:05 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import myReducer from './reducers';
+import Results from './components/results';
+
+let store = createStore(myReducer);
+
+function render() {
+    ReactDOM.render(
+        <div className='container'>
+            <App store={store}/>
+            <hr />
+            <Results store={store}/>
+        </div>,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(render);
+
+render();
